b9b677764c11a8019370cd0ebabfa19b248feaf0
Evandro Araújo <evandroqa@gmail.com>
2026-03-16 22:46:22 -0300

    Tratamento de Exceções

--- backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
@@ -1,13 +1,16 @@
 package com.devsuperior.DSCatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DSCatalog.dto.CategoryDTO;
+import com.devsuperior.DSCatalog.entities.Category;
 import com.devsuperior.DSCatalog.repositories.CategoryRepository;
+import com.devsuperior.DSCatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -18,6 +21,13 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		return repository.findAll().stream().map(x -> new CategoryDTO(x)).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Não Encontrado - (classe: CategoryService)"));
+		return new CategoryDTO(entity);
 	}
 
 }
---
+++ backend/src/main/java/com/devsuperior/DSCatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.DSCatalog.services.exceptions;
+
+@SuppressWarnings("serial")
+public class EntityNotFoundException extends RuntimeException{
+
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
